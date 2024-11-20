2a10779b2c55cf3aabb9fbc2d6e90ddeb1393ec4
mi-aw <your-username@users.noreply.github.com>
2024-11-20 23:01:03 +0000

    tweaking the randomizer

--- game.py
+++ game.py
@@ -397,8 +397,8 @@ class Game:
 
     def draw_move_counter(self):
         font = pygame.font.SysFont(None, 40)
-        move_text = font.render(f'Moves: {self.move_count}', True, (0, 0, 0))
-        self.screen.blit(move_text, (self.screen_width - 150, 20))
+        move_text = font.render(f'Moves: {self.move_count}', True, (255, 255, 255))
+        self.screen.blit(move_text, (self.screen_width - 250, 20))
 
 if __name__ == "__main__":
     game = Game()
